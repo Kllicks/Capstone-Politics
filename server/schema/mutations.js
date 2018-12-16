f8785a29915d1b3810f3fc167f57e2154abd9c6a
hharris0803 <hchang0803@outlook.com>
2018-12-16 12:23:35 -0500

    add logout mutation- after check authentication

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -27,8 +27,24 @@ const mutation = new GraphQLObjectType({
                 //returns a promise - when return a promise from resolve function need to make sure to return it - so graphql knows to look at the promise that gets returned to know to wait for operation to resolve before attempting to return any values to front end. 
                 return AuthService.signup({ email, password, req });
             }
+        },
+        //logout mutation 
+        logout: {
+            type: UserType, 
+            resolve(parentValue, args, req) {
+                // req.logout removes the user property off the request object
+                //so first save a ref to the user property then logout the user and then return the user.
+                //aka getting passport and graphql to mesh together since passport is what is handling the signout of the user- but we need to instruct it to do so
+                //optionally to clean up code could add another function to the auth service and move logic below into it. (still need to return user from that logout function)
+                const { user } = req;
+                req.logout();
+                return user;
+            }
         }
     }
 });
 
 module.exports = mutation;
+
+
+
