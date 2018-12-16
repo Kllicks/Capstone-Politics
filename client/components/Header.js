7bb3e9a8258a3143523940c82fb4105488f3300c
hharris0803 <hchang0803@outlook.com>
2018-12-16 14:03:53 -0500

    links to signup and login in header

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
@@ -12,17 +13,29 @@ class Header extends Component {
             return <div>Logout</div>;
         } else {
             return (
-                <div>You are not signed in.</div>
+                <div>
+                    <li>
+                        <Link to="/signup">Signup</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </div>
             );
         }
     }
-    
+
     render() {
         // console.log(this.props.data);
         return (
             <nav>
                 <div className="nav-wrapper">
-                    {this.renderButtons()}
+                    <Link to="/" className="brand-logo left">
+                        Home
+                    </Link>
+                    <ul className="right">
+                        {this.renderButtons()}
+                    </ul>
                 </div>
             </nav>
         );
