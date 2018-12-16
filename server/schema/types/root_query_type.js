0426841d8bdc5ff8b3d56ef006c2784fcf54fcc9
hharris0803 <hchang0803@outlook.com>
2018-12-16 12:47:49 -0500

    add authentication

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,11 +1,19 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLID } = graphql;
+const UserType = require('./user_type');
 
 const RootQueryType = new GraphQLObjectType({
   //by default graphql expects to have at least one field available for every single defined type 
   name: 'RootQueryType',
   fields: {
-    dummyField: { type: GraphQLID }
+    user: {
+      type: UserType,
+      //instruct graphql how to return that current user
+      resolve(parentValue, args, req) {
+        //if signed in return them - otherwise- return undefined/null
+        return req.user;
+      }
+    }
   }
 });
 
