1c85f1bd48fca6f4cb95e0b7d4037d9cd950ba91
hharris0803 <hchang0803@outlook.com>
2018-12-16 14:56:15 -0500

    submission of auth form

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -7,21 +7,29 @@ class AuthForm extends Component {
         this.state = { email: '', password: ''};
     }
 
+    onSubmit(event) {
+        event.preventDefault();
+
+        //never going to mutate the object inside callback itself so this is fine
+        this.props.onSubmit(this.state);
+    }
+
     render() {
         return (
             //css for layout of the form
             <div className="row">
-                <form className="col s4">
+                <form onSubmit={this.onSubmit.bind(this)} className="col s4">
                     <div className="input-field">
-                        <label>Email</label>
                         <input 
+                            placeholder="Email"
                             value={this.state.email}
                             onChange={e => this.setState({ email: e.target.value })}
                         />
                     </div>
                     <div className="input-field">
-                        <label>Password</label>
                         <input 
+                            placeholder="Password"
+                            type="password"
                             value={this.state.password}
                             onChange={e => this.setState({ password: e.target.value })}
                         />
